c69933b59e523707b4bd656d6f048791cefda8ad
roussebe <roussebe@gmail.com>
2021-12-10 13:15:56 +0100

    first commit

---
+++ srv/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+
+app.get( '/', (req, res) => {
+	res.send("Hello Node.js on App Engine Standard!");
+});
+
+const server = app.listen( process.env.PORT || 8080, () => {
+	const host = server.address().address;
+	const port = server.address().port;
+	console.log( "Example app listening" )
+});
+
