5acfb8cb082270fb81e8f63f33604bb39d2ae860
roussebe <roussebe@gmail.com>
2021-12-30 15:13:18 +0100

    Mongoose

--- srv/server.js
+++ srv/server.js
@@ -1,13 +1,120 @@
+const dotenv = require('dotenv')
+
 const express = require('express')
+const session = require('express-session')
+//const cookieParser = require( 'cookie-parser' );
+const morgan = require('morgan');
+const path = require('path')
+const methodOverride = require('method-override')
+//const NedbStore = require('connect-nedb-session')(session);
+
+const MongoStore = require('connect-mongo')
+const connectDB = require('./config/db')
+
+const passport = require('passport');
+const {create} = require('express-handlebars')
+
+console.log( "Starting up" )
+
+
+dotenv.config( {path: './srv/config/config.env' })
+
+require( './config/passport' )(passport)
+const PORT = process.env.PORT || 8080
+
+
+connectDB()
+
 const app = express();
+const NODE_ENV = process.env.NODE_ENV || "development"
 
-app.get( '/', (req, res) => {
-	res.send("Hello Node.js on App Engine Standard!");
-});
 
-const server = app.listen( process.env.PORT || 8080, () => {
-	const host = server.address().address;
-	const port = server.address().port;
-	console.log( "Example app listening" )
-});
+if( NODE_ENV === 'development') {
+    app.use( morgan( 'dev' ) )
+}
 
+// Body parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+// Method override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+  })
+)
+
+// Handlebars
+const {
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select,
+} = require('./helpers/hbs')
+
+const hbs = create({
+     helpers: {
+      formatDate,
+      stripTags,
+      truncate,
+      editIcon,
+      select,
+    },
+    defaultLayout: 'simple',
+    extname: '.hbs',
+ })
+
+app.engine( '.hbs', hbs.engine )
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views') )
+
+console.log( path.join(__dirname, 'views') )
+
+// Session & Passport
+console.log( process.env.MONGO_URI )
+app.use(session({ secret: 'yoursecret'
+                   , resave: false
+                   , saveUninitialized: false
+                   , cookie: { path: '/'
+                             , httpOnly: true
+                             , maxAge: 365 * 24 * 3600 * 1000   // One year for example
+                             }
+                   , store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
+                   }));
+console.log( "App user done" )
+ //dbStore({ filename: path.join(__dirname, '../DB/session.nedb') })
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Set global var
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null
+  next()
+})
+
+//Routes
+app.use(express.static(path.join(__dirname, '../public')));
+app.use('/', require('./routes/index' ))
+app.use('/auth', require('./routes/auth' ))
+app.use('/stories', require('./routes/stories' ))
+app.use('/rituals', require('./routes/rituals' ))
+app.use('/users', require('./routes/users' ))
+app.use('/habits', require('./routes/habits' ))
+
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null
+  next()
+})
+
+const server = app.listen( PORT, () => {
+	console.log( "Server Listening on " + PORT )
+})
+
+console.log( "Done" )
