78671f2f7c855e12f2c696527b382a17c3ff94d3
roussebe <roussebe@gmail.com>
2021-12-31 17:38:48 +0100

    changing style - adding basic profile

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,57 @@
-document.addEventListener('DOMContentLoaded', function() {
+let app_context = {}
+
+document.addEventListener('DOMContentLoaded', async function() {
+  console.log( "Event Listener" )
   // nav menu
   const menus = document.querySelectorAll('.side-menu');
   M.Sidenav.init(menus, {edge: 'left'});
   // add recipe form
   const forms = document.querySelectorAll('.side-form');
   M.Sidenav.init(forms, {edge: 'right'});
+
+  let profile = await $.get('/api/basic_profile');
+  console.log( profile )
+
+  let content = ""
+
+  console.log( $("#rituals").html() )
+  for( let r of profile.rituals ) {
+    content += `<div class="white-text text-darken-1 pk-contact">
+          <div class="contact-image">
+            <img src="img/pkcontacts.png" alt="contact thumb">
+          </div>
+          <div class="contact-details">
+            <div class="contact-title">${r.title}</div>
+            <div class="contact-numbers">+130902092309</div>
+          </div>
+          <div class="contact-options">
+            <i class="material-icons">call</i>
+            <i class="material-icons">delete_outline</i>
+          </div>
+        </div>`
+    }
+
+    $("#rituals").html( content )
 });
+
+
+
+
+
+function ajaxPost( url, data, next ) {
+  console.log( url )
+  console.log( data )
+
+  $.ajax( {
+    type: "POST",
+    dataType:"json",
+    contentType: "application/json",
+    url: url,
+    data: JSON.stringify(data)
+  })
+  .done( next )
+  .fail(function(xhr, textStatus, errorThrown) {
+    console.log("ERROR: ",xhr.responseText)
+    return xhr.responseText;
+  })
+}
--- srv/routes/index.js
+++ srv/routes/index.js
@@ -14,7 +14,8 @@ router.get('/dashboard', ensureAuth, async (req, res) => {
         const rituals = await Ritual.find( { user: req.user._id } ).lean()
         const user = await User.findOne( {_id: req.user._id })
         console.log( "Rituals" , rituals )
-        res.render( 'dashboard', {
+        res.render( 'dashboard_new', {
+            layout: 'basic',
             name: req.user.firstName,
             usr: user,
             rituals,
@@ -22,6 +23,25 @@ router.get('/dashboard', ensureAuth, async (req, res) => {
     } catch( err ) {
         res.render('error/500')
     }
+})
+
+router.get('/api/basic_profile', ensureAuth, async(req, res) => {
+  console.log("Getting basic profile for ", req.user )
+  try {
+    const rituals = await Ritual.find( { user: req.user._id } ).lean()
+    const user = await User.findOne( { _id: req.user._id } )
+
+    let obj = {
+      name: user.firstName,
+      id: user._id,
+      rituals: rituals.map( r => { return { title: r.title, time: r.time } } )
+    }
+
+    res.send( obj )
+
+  } catch( err ) {
+    res.status( "500" ).send()
+  }
 })
 
 module.exports = router
